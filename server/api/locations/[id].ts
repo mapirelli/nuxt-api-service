0166b4540c98c7fba9d9f8a1b09301588bf5155b
Mariano Pirelli <mapirelli@gmail.com>
2024-10-21 19:52:37 +0200

    chore: Access-Control-Allow-Origin

--- server/api/locations/[id].ts
+++ server/api/locations/[id].ts
@@ -1,6 +1,7 @@
 import db from '../../../db.json'
 
 export default defineEventHandler((event) => {
+  setHeader(event, 'Access-Control-Allow-Origin', 'https://mapirelli-angular-real-estate.netlify.app')
   const id = getRouterParam(event, 'id')
   if (id) {
     return db.locations.find(location => location.id === Number(id))
--- server/api/locations/index.ts
+++ server/api/locations/index.ts
@@ -1,5 +1,6 @@
 import db from '../../../db.json'
 
 export default defineEventHandler((event) => {
+  setHeader(event, 'Access-Control-Allow-Origin', 'https://mapirelli-angular-real-estate.netlify.app')
   return db.locations
 })
